bac6f2ebc612d85e27a61b747b1f308803e55b42
doctype-Melvin <doctype.melvin@gmail.com>
2022-04-08 08:41:48 +0200

    new layout includes popup form

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+let grid = document.querySelector('.grid');
+let addBook = document.querySelector('.add');
+let deleteAll = document.querySelector('.deleteAll');
+let popAdd = document.getElementById('pop-add')
+
+//Book prototype
 function Book(title, author, pages){
     this.title = title,
     this.author = author,
-    this.pages = pages,
+    this.pages = pages
+}
+
+//Storage for book input
+let library = [];
+
+//Link popup to addBook button
+addBook.addEventListener('click', openPopUp);
+popAdd.addEventListener('click', closePopUp);
+
+function openPopUp() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+function closePopUp() {
+    document.getElementById('popup').style.display = 'none';
 }
